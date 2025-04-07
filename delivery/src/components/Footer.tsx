e53a916d0eef4274e2755ccbaf452d1cc852977a
Habtamu <195909267+Habte121212@users.noreply.github.com>
2025-04-07 13:35:28 +0300

    Footer

--- delivery/src/components/Footer.tsx
+++ delivery/src/components/Footer.tsx
@@ -1,9 +1,11 @@
+import Link from 'next/link'
 import React from 'react'
 
 const Footer = () => {
   return (
-    <div>
-      Fotter
+    <div className="h-12 md:h-24 p-4 lg:p-20 xl:p-40 text-red-500 flex items-center justify-between">
+      <Link href="/" className='font-bold text-xl'>MASSIMO</Link>
+      <p>&copy; ALL RIGHTS RESERVED, 2025</p>
     </div>
   )
 }
