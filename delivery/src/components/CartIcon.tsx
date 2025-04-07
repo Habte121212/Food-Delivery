0b6f60d50f28707702d1477206a2a9260e6aab33
Habtamu <195909267+Habte121212@users.noreply.github.com>
2025-04-07 13:10:20 +0300

    header with different screen

--- delivery/src/components/CartIcon.tsx
+++ delivery/src/components/CartIcon.tsx
@@ -4,7 +4,7 @@ import React from 'react'
 const CartIcon = () => {
   return (
     <div className="flex items-center gap-4 space-x-2">
-      <div className="relative w-8 h-8">
+      <div className="relative w-8 h-8 md:w-5 md:h-5">
         <Image
           src="/cart.png"
           alt="Cart Icon"
--- delivery/src/components/Navbar.tsx
+++ delivery/src/components/Navbar.tsx
@@ -1,17 +1,38 @@
 import React from 'react'
 import Menu from './Menu'
 import Link from 'next/link'
+import CartIcon from './CartIcon'
+import Image from 'next/image'
 
 const Navbar = () => {
+  const user = false
   return (
-    <div className='h-12 flex text-red-500 items-center p-4 justify-between  border-b-2 border-b-red-500 uppercase'>
+    <div className="h-12 flex text-red-500 items-center p-4 justify-between  border-b-2 border-b-red-500 uppercase md:24 lg:px-20 xl:px-40">
+                 {/* lift link */}
+      <div className="hidden md:flex gap-4">
+        <Link href="/">HomePage</Link>
+        <Link href="/menu">Menu</Link>
+        <Link href="/">Contact</Link>
+      </div>
       {/* logo */}
-      <div className= ' text-xl'>
+      <div className=" text-xl font-bold flex-1 text-center">
         <Link href="/">Massimo</Link>
       </div>
       {/* mobile menu */}
-      <div>
+      <div className="md:hidden">
         <Menu />
+      </div>
+             {/* right link */}
+      <div className="hidden md:flex gap-4 items-center justify-end flex-1">
+        <div className='md:absolute top-3 r-2 lg:static flex items-center gap-2 cursor-pointer bg-orange-300 px-1 rounded-md'>
+          <Image src="/phone.png" alt="" width={20} height={20}/>
+          <span>+251 945 87 07 00 </span>
+        </div>
+        {!user ? (
+          <Link href="/login">Login</Link>
+        ):(
+        <Link href="/order">Oeders</Link>)}
+         <CartIcon/>
       </div>
     </div>
   )
